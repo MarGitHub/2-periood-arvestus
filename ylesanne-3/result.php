d6b1f4aa87efa9cb8b6a6ad2cb8720fa9f4efc91
Margit <margit.pilden@gmail.com>
2014-11-06 09:40:12 +0200

    Add ÜL3 - not completed

--- ylesanne-3/result.php
+++ ylesanne-3/result.php
@@ -8,6 +8,23 @@
   <body>
     <!-- Kustuta see funktsioon (print_r() koos <pre> elemendiga) pärast seda, kui oled veendunud, et programm töötab. -->
     <pre><?php print_r($_POST); ?></pre>
+	<?php 
+	
+	
+	if ($username = $_POST ["username"] && $_POST["sex"]){
+	echo "Tere, {$username}, tundub, et olete {$sex}";
+	}
+	elseif (empty($sex) ) {
+	echo	"Palun minge tagasi ja valige sugu."; 
+	}
+	elseif (empty($username)) {
+	echo "Palun minge tagasi ja sisestage nimi.";
+	}
+	elseif (empty($username) && empty($sex)) {
+	echo "Palun minge tagasi ja täitke väljad.";
+	}
+	
+	?>
 
     <div><a href="index.php">Mine tagasi</a></div>
   </body>
